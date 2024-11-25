e8543ce8dda9fedf611cce896444c78174a713ef
sofckit <amalichkaaaa@yandex.ru>
2024-11-25 12:04:40 +0300

    Добавил миграции

--- oil_loss_system/oil_losses/models.py
+++ oil_loss_system/oil_losses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class OilLossRecord(models.Model):
+    date = models.DateField(verbose_name="Дата")
+    location = models.CharField(max_length=255, verbose_name="Местоположение")
+    oil_type = models.CharField(max_length=100, verbose_name="Тип нефти или нефтепродукта")
+    estimated_loss = models.FloatField(verbose_name="Оценочная потеря (тонн)")
+    actual_loss = models.FloatField(verbose_name="Фактическая потеря (тонн)")
+
+    def __str__(self):
+        return f"{self.date} - {self.location} ({self.oil_type})"
+
+    class Meta:
+        verbose_name = "Запись о потере нефти"
+        verbose_name_plural = "Записи о потерях нефти"
