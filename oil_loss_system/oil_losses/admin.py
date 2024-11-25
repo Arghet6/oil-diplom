91ccd23abd343ecc2c8523f4ea2c740c2456a682
sofckit <amalichkaaaa@yandex.ru>
2024-11-25 12:17:20 +0300

    Добавил модель таблицы для вывода оценочных и фактических потерь нефтепродуктов

--- oil_loss_system/oil_losses/admin.py
+++ oil_loss_system/oil_losses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OilLossRecord
 
-# Register your models here.
+@admin.register(OilLossRecord)
+class OilLossRecordAdmin(admin.ModelAdmin):
+    list_display = ('date', 'location', 'oil_type', 'estimated_loss', 'actual_loss')
+    search_fields = ('location', 'oil_type')
+    list_filter = ('date', 'oil_type')
--- oil_loss_system/oil_losses/views.py
+++ oil_loss_system/oil_losses/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import OilLossRecord
 
-# Create your views here.
+def index(request):
+    records = OilLossRecord.objects.all()
+    return render(request, 'index.html', {'records': records})
