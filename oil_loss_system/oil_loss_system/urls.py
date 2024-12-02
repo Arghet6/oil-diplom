22d7899c927b1ab9baa78bb8cb1e1c218d8e4716
sofckit <amalichkaaaa@yandex.ru>
2024-12-02 09:18:19 +0300

    Сделал главную страницу, вход и регистрацию

--- oil_loss_system/oil_loss_system/urls.py
+++ oil_loss_system/oil_loss_system/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from oil_losses.forms import UserRegisterForm
+from oil_losses.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oil_losses.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- oil_loss_system/oil_losses/views.py
+++ oil_loss_system/oil_losses/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import OilLossRecord
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from .forms import OilLossRecordForm
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+
 
 def index(request):
     oil_type = request.GET.get('oil_type')
@@ -42,3 +45,20 @@ def edit_record(request, pk):
     else:
         form = OilLossRecordForm(instance=record)
     return render(request, 'edit.html', {'form': form, 'record': record})
+
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    return render(request, 'welcome.html')  # Показывает "Здарова..." для гостей
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Автоматически вход после регистрации
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
